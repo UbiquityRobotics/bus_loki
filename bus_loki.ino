ea664dc10c2461a720e5645981ac0016ab3ac301
Wayne C. Gramlich <Wayne@Gramlich.Net>
2015-05-12 13:28:41 -0700

    Got Loki encoders working.

--- bus_loki.ino
+++ bus_loki.ino
@@ -19,23 +19,73 @@
 
 class Loki_Motor_Encoder : Bus_Motor_Encoder {
  public:
+  Loki_Motor_Encoder(UByte input1_pin, UByte input2_pin,
+   UByte enable_pin, Integer *encoder_pointer);
   Integer encoder_get();
   void encoder_set(Integer encoder);
   void pwm_set(Byte pwm);
+ private:
+  UByte _input1_pin;
+  UByte _input2_pin;
+  UByte _enable_pin;
+  Integer *_encoder_pointer;
 };
 
-void Loki_Motor_Encoder::pwm_set(Byte pwm) {
-  // Do something here:
-}
+// Encoder buffer:
+#define BUFFER_POWER 8
+#define BUFFER_SIZE (1 << BUFFER_POWER)
+#define BUFFER_MASK (BUFFER_SIZE - 1)
+static UByte encoder_buffer[BUFFER_SIZE];
+static volatile UByte encoder_buffer_in = 0;
+static volatile UByte encoder_buffer_out = 0;
 
-Integer Loki_Motor_Encoder::encoder_get() {
-  // Do something here:
-  return 0;
-}
+// There are two encoders:
+//
+//    Encoder   Phase  Arduino Pin   PCInt
+//    =======   =====  ===========   =======
+//      1	  A        A0        PCInt8
+//      1         B        A1        PCInt9
+//      2         A        A2        PCInt10
+//      2         B        A3        PCInt11
 
-void Loki_Motor_Encoder::encoder_set(Integer encoder) {
-  // Do something here:
-}
+// This table is computed by bus_bridge_encoders_sonar.py
+// Please read the comment in the Python code to understand
+// the structure of the *state_transition_table* below:
+static Byte state_transition_table[32] = {
+    //            ccccc         sgg   // [xx]: SGG[gg] => sgg +/-
+    (signed char)((0x00 << 3) | 0x0), // [00]: 000[00] => 000  0
+    (signed char)((0x1f << 3) | 0x4), // [01]: 001[00] => 100 -1
+    (signed char)((0x01 << 3) | 0x0), // [02]: 010[00] => 000 +1
+    (signed char)((0x02 << 3) | 0x0), // [03]: 011[00] => 000 +2
+    (signed char)((0x00 << 3) | 0x4), // [04]: 100[00] => 100  0
+    (signed char)((0x1f << 3) | 0x4), // [05]: 101[00] => 100 -1
+    (signed char)((0x01 << 3) | 0x0), // [06]: 110[00] => 000 +1
+    (signed char)((0x1e << 3) | 0x4), // [07]: 111[00] => 100 -2
+    (signed char)((0x01 << 3) | 0x1), // [08]: 1000[01] => 001 +1
+    (signed char)((0x00 << 3) | 0x1), // [09]: 1001[01] => 001  0
+    (signed char)((0x02 << 3) | 0x1), // [10]: 1010[01] => 001 +2
+    (signed char)((0x1f << 3) | 0x5), // [11]: 1011[01] => 101 -1
+    (signed char)((0x01 << 3) | 0x1), // [12]: 1100[01] => 001 +1
+    (signed char)((0x00 << 3) | 0x5), // [13]: 1101[01] => 101  0
+    (signed char)((0x1e << 3) | 0x5), // [14]: 1110[01] => 101 -2
+    (signed char)((0x1f << 3) | 0x5), // [15]: 1111[01] => 101 -1
+    (signed char)((0x1f << 3) | 0x6), // [16]: 10000[10] => 110 -1
+    (signed char)((0x02 << 3) | 0x2), // [17]: 10001[10] => 010 +2
+    (signed char)((0x00 << 3) | 0x2), // [18]: 10010[10] => 010  0
+    (signed char)((0x01 << 3) | 0x2), // [19]: 10011[10] => 010 +1
+    (signed char)((0x1f << 3) | 0x6), // [20]: 10100[10] => 110 -1
+    (signed char)((0x1e << 3) | 0x6), // [21]: 10101[10] => 110 -2
+    (signed char)((0x00 << 3) | 0x6), // [22]: 10110[10] => 110  0
+    (signed char)((0x01 << 3) | 0x2), // [23]: 10111[10] => 010 +1
+    (signed char)((0x02 << 3) | 0x3), // [24]: 11000[11] => 011 +2
+    (signed char)((0x01 << 3) | 0x3), // [25]: 11001[11] => 011 +1
+    (signed char)((0x1f << 3) | 0x7), // [26]: 11010[11] => 111 -1
+    (signed char)((0x00 << 3) | 0x3), // [27]: 11011[11] => 011  0
+    (signed char)((0x1e << 3) | 0x7), // [28]: 11100[11] => 111 -2
+    (signed char)((0x01 << 3) | 0x3), // [29]: 11101[11] => 011 +1
+    (signed char)((0x1f << 3) | 0x7), // [30]: 11110[11] => 111 -1
+    (signed char)((0x00 << 3) | 0x7), // [31]: 11111[11] => 111  0
+};
 
 // Define pin numbers:
 static const int encoder_r1_pin = 10;		// IC Pin 23
@@ -50,6 +100,7 @@ static const int led4_pin = 34;
 static const int led5_pin = 35;
 static const int led6_pin = 36;
 static const int led7_pin = 37;
+static const int miso_pin = 50;
 static const int motor1_input1_pin = 43;
 static const int motor1_input2_pin = 44;
 static const int motor1_enable_pin = 45;
@@ -71,32 +122,59 @@ AVR_UART *bus_uart = &avr_uart1;
 AVR_UART *debug_uart = &avr_uart0;
 AVR_UART *host_uart = &avr_uart0;
 
+// The two encoder values:
+Integer encoder1 = 0;
+Integer encoder2 = 0;
+
 Bus_Slave bus_slave((UART *)bus_uart, (UART *)host_uart);
 
-Loki_Motor_Encoder left_motor_encoder, right_motor_encoder;
+Loki_Motor_Encoder left_motor_encoder(motor1_input1_pin, motor1_input2_pin,
+ motor1_enable_pin, &encoder1);
+Loki_Motor_Encoder right_motor_encoder(motor2_input1_pin, motor2_input2_pin,
+ motor2_enable_pin, &encoder2);
+
 Bridge bridge(&avr_uart0, &avr_uart1, &avr_uart0, &bus_slave,
  (Bus_Motor_Encoder *)&left_motor_encoder,
  (Bus_Motor_Encoder *)&right_motor_encoder);
 
 void leds_byte_write(char byte) {
-    digitalWrite(led0_pin, (byte & 1) ? LOW : HIGH);
-    digitalWrite(led1_pin, (byte & 2) ? LOW : HIGH);
-    digitalWrite(led2_pin, (byte & 4) ? LOW : HIGH);
-    digitalWrite(led3_pin, (byte & 8) ? LOW : HIGH);
-    digitalWrite(led4_pin, (byte & 16) ? LOW : HIGH);
-    digitalWrite(led5_pin, (byte & 32) ? LOW : HIGH);
-    digitalWrite(led6_pin, (byte & 64) ? LOW : HIGH);
-    digitalWrite(led7_pin, (byte & 128) ? LOW : HIGH);
+  //digitalWrite(led0_pin, (byte & 1) ? LOW : HIGH);
+  //digitalWrite(led1_pin, (byte & 2) ? LOW : HIGH);
+  //digitalWrite(led2_pin, (byte & 4) ? LOW : HIGH);
+  //digitalWrite(led3_pin, (byte & 8) ? LOW : HIGH);
+  //digitalWrite(led4_pin, (byte & 16) ? LOW : HIGH);
+  //digitalWrite(led5_pin, (byte & 32) ? LOW : HIGH);
+  //digitalWrite(led6_pin, (byte & 64) ? LOW : HIGH);
+  //digitalWrite(led7_pin, (byte & 128) ? LOW : HIGH);
+  PORTC = byte;
 }
 
+// *PCINT0_vect*() is the interrupt service routine for the
+// pin change interrupts PCINT7/.../0.  The two encoders are
+// attached to PCINT7/6/5/4, so these are the bits we want
+// to capture.  This routine just stuffs the encoder bits
+// into a buffer:
+
+ISR(PCINT0_vect) {
+  // Stuff the port C input bits into *buffer* and advance *buffer_in*:
+  UByte bits = PINB;
+  encoder_buffer[encoder_buffer_in] = bits;
+  encoder_buffer_in = (encoder_buffer_in + 1) & BUFFER_MASK;
+  PORTC = PINB;
+  //leds_byte_write(bits);
+  //leds_byte_write(encoder_buffer_in);
+  //host_uart->print("0123456789abcdef"[(bits >> 4) & 0xf]);
+  //UDR0 = '.';
+}
 
 // The *setup*() routine runs once when you press reset:
-void setup() {                
+void setup() {
   // Initialize pin directions:
+  pinMode(encoder_l1_pin, INPUT);
+  pinMode(encoder_l2_pin, INPUT);
   pinMode(encoder_r1_pin, INPUT);
   pinMode(encoder_r2_pin, INPUT);
-  pinMode(encoder_l1_pin, INPUT);
-  pinMode(encoder_r2_pin, INPUT);
+  pinMode(miso_pin, INPUT);
   pinMode(led0_pin, OUTPUT);
   pinMode(led1_pin, OUTPUT);
   pinMode(led2_pin, OUTPUT);
@@ -125,10 +203,29 @@ void setup() {
   //host_uart->print((Text)"Double echo\r\n");
 
   switch (BUS_LOKI_PROGRAM) {
+    case BUS_LOKI_PROGRAM_ENCODERS_TO_LEDS: {
+      PCMSK0 = _BV(7) | _BV(6) | _BV(5) | _BV(4);
+      PCICR = _BV(0);
+      break;
+    }
     case BUS_LOKI_PROGRAM_RAB: {
       host_uart->print((Text)"Start bridge setup\r\n");
       bridge.setup(TEST);
       host_uart->print((Text)"Bridge setup done\r\n");
+
+      // Set up Interrupt on Pin Change interrupt vector.  The encoder
+      // pins are attached to PCINT7/6/5/4, so we only need to set
+      // PCMSK0 to '1111 0000':
+      PCMSK0 = _BV(7) | _BV(6) | _BV(5) | _BV(4);
+
+      // Now enable interrup on changes for PCINT7/.../0, by setting
+      // PCICR to 1.  Thus, PCICR is set to 'xxxx x001' or '0000 0001':
+      PCICR = _BV(0);
+
+      // Enable global interrupts by setting the I bit (7th bit) in the
+      // status register:
+      //SREG |= _BV(7);
+
       break;
     }
   }
@@ -193,16 +290,57 @@ void loop() {
     }
     case BUS_LOKI_PROGRAM_ENCODERS_TO_LEDS: {
       // Read the encoder values:
-      char encoder_r1 = digitalRead(encoder_r1_pin);
-      char encoder_r2 = digitalRead(encoder_r2_pin);
-      char encoder_l1 = digitalRead(encoder_l1_pin);
-      char encoder_l2 = digitalRead(encoder_l2_pin);
+      if (0) {
+	Logical encoder_r1 = (Logical)0;
+        Logical encoder_r2 = (Logical)0;
+	Logical encoder_l1 = (Logical)0;
+	Logical encoder_l2 = (Logical)0;
+	UByte bits = 0;
+	if (1) {
+	  encoder_r1 = digitalRead(encoder_r1_pin);
+	  encoder_r2 = digitalRead(encoder_r2_pin);
+	  encoder_l1 = digitalRead(encoder_l1_pin);
+	  encoder_l2 = digitalRead(encoder_l2_pin);
+	} else {
+	  bits = PINB;
+	  if ((bits & 0x10) != 0) {
+	    encoder_r1 = (Logical)1;
+	  }
+	  if ((bits & 0x20) != 0) {
+	    encoder_r2 = (Logical)1;
+	  }
+	  if ((bits & 0x40) != 0) {
+	    encoder_l1 = (Logical)1;
+	  }
+	  if ((bits & 0x80) != 0) {
+	    encoder_l2 = (Logical)1;
+	  }
+	}
 
-      // Write the encoder values:
-      digitalWrite(led0_pin, encoder_r1);
-      digitalWrite(led1_pin, encoder_r2);
-      digitalWrite(led3_pin, encoder_l1);
-      digitalWrite(led4_pin, encoder_l2);
+	if (1) {
+	  char leds = 0;
+          if (encoder_r1) {
+	  leds |= 1;
+	  }
+	  if (encoder_r2) {
+	    leds |= 2;
+	  }
+	  if (encoder_l1) {
+	    leds |= 0x40;
+	  }
+	  if (encoder_l2) {
+	    leds |= 0x80;
+	  }
+	  PORTC = leds;
+	} else {
+	  digitalWrite(led0_pin, encoder_r1);
+ 	  digitalWrite(led1_pin, encoder_r2);
+	  digitalWrite(led6_pin, encoder_l1);
+	  digitalWrite(led7_pin, encoder_l2);
+	}
+      } else {
+	PORTC = PINB;
+      }
       break;
     }
     case BUS_LOKI_PROGRAM_LEDS_COUNT: {
@@ -241,8 +379,90 @@ void loop() {
       break;
     }
     case BUS_LOKI_PROGRAM_RAB: {
+      static UByte encoder1_state = 0;
+      static UByte encoder2_state = 0;
+
+      // Deal with encoder issues:
+      if (encoder_buffer_in != encoder_buffer_out) {
+	// Grab all 4 encoder bits from *encoder_buffer*.  The 4 bits are
+	// in the high order nibble and need to be shifted down to the low
+	// order nibble:
+	UByte encoder_bits = encoder_buffer[encoder_buffer_out] >> 4;
+	encoder_buffer_out = (encoder_buffer_out + 1) & BUFFER_MASK;
+
+	// Just print out *encoder_bits*:
+	//Serial.print(encoder_bits & 0x3);
+	//Serial.print(":");
+	//Serial.print((encoder_bits >> 2) & 0x3);
+	//Serial.print("\n");
+
+	// Process encoder1:
+	// *encoder1_state* has 3 bits of state - '0000 0sss'.
+	// We take encoder1 bits encoder from *encoder_bits* ('0000 00ee'),
+	// shift them left by 3, and OR them with the *encoder1_state*:
+	//
+	//     encoder1_state:  0000 0sss
+	//     encoder_bits<<3: 000e e000
+	//  OR ==========================
+	//     index            000e esss
+	//
+	UByte index = ((encoder_bits & 0x3) << 3) | encoder1_state;
+
+	// Note that *state_transition* is signed.
+	Byte state_transition = state_transition_table[index];
+	encoder1 += state_transition >> 3;
+	encoder1_state = (unsigned char)(state_transition & 0x7);
+
+	// Process encoder2:
+	// Now we do the same for encoder2 whose bits are 2 bits over
+	// ('0000 eexx'):
+	index = ((encoder_bits & 0xc) << 1) | encoder2_state;
+	state_transition = state_transition_table[index];
+	encoder2 += state_transition >> 3;
+	encoder2_state = (unsigned char)(state_transition & 0x7);
+      }
+
       bridge.loop(TEST);
       break;
     }
   }
 }
+
+// *Loki_Motor_Encoder* classes:
+
+Loki_Motor_Encoder::Loki_Motor_Encoder(UByte input1_pin, UByte input2_pin,
+ UByte enable_pin, Integer *encoder_pointer) {
+  _input1_pin = input1_pin;
+  _input2_pin = input2_pin;
+  _enable_pin = enable_pin;
+  _encoder_pointer = encoder_pointer;
+}
+
+void Loki_Motor_Encoder::pwm_set(Byte pwm) {
+  UByte input1 = LOW;
+  UByte input2 = LOW;
+  UByte enable_pwm = 0;
+  if (pwm > 0) {
+    enable_pwm = ((UByte)pwm << 1) | 1;
+    input1 = HIGH;
+  } else if (pwm < 0) {
+    enable_pwm = (((UByte)(-pwm)) << 1) | 1;
+    input2 = HIGH;
+  }
+
+  // Set the direction pins and pulse the output:
+  digitalWrite(_input1_pin, input1);
+  digitalWrite(_input2_pin, input2);
+  analogWrite(_enable_pin, enable_pwm);
+}
+
+Integer Loki_Motor_Encoder::encoder_get() {
+  // Do something here:
+  return *_encoder_pointer;
+}
+
+void Loki_Motor_Encoder::encoder_set(Integer encoder) {
+  // Do something here:
+  *_encoder_pointer = encoder;
+}
+
